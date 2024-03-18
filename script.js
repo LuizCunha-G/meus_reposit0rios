3052b89aab32f94f91201e542fc249cd8faa6faa
LuizCunha-G <160529066+LuizCunha-G@users.noreply.github.com>
2024-03-18 17:41:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Temperature data
+const temperatureData = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    datasets: [{
+        label: 'Temperature',
+        data: [12, 19, 3, 5, 2, 3, 7, 8, 9, 10, 11, 12],
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1
+    }]
+};
+
+// Energy data
+const energyData = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    datasets: [{
+        label: 'Energy',
+        data: [120, 190, 130, 150, 120, 130, 170, 180, 190, 200, 210, 220],
+        backgroundColor: 'rgba(153, 102, 255, 0.2)', // Add the alpha value here
+        borderColor: 'rgba(153, 102, 255, 1)',
+        borderWidth: 1
+    }]
+};
